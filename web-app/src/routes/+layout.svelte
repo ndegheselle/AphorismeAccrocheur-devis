82725963901d47470b09fb817659f9912caa690b
Nicolas de gheselle <nicolas@degheselle.fr>
2025-03-20 19:19:11 +0100

    Updated all packages

--- web-app/src/routes/+layout.svelte
+++ web-app/src/routes/+layout.svelte
@@ -1,6 +1,4 @@
 <script>
-    import "@fortawesome/fontawesome-free/css/all.min.css";
-    import "../app.css";
     import Navigation from "./navigation.svelte";
     import Footer from "./footer.svelte";
 </script>
