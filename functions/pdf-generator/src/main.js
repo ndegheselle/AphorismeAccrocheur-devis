97887e4d507336955933fbf2367120694233901f
NicolasDeGheselle <nicolas@degheselle.fr>
2025-04-22 14:44:50 +0200

    Chabge Change to test file

--- functions/pdf-generator/src/main.js
+++ functions/pdf-generator/src/main.js
@@ -1,12 +1,12 @@
 import { generateFakeOrder } from './faker.js';
-import { createPdf } from './pdf.js';
 
 export default async ({ res, log }) => {
   const fakeOrder = generateFakeOrder();
   log(`Generated fake order: ${JSON.stringify(fakeOrder, null, 2)}`);
-
-  const pdfBuffer = await createPdf(fakeOrder);
-  log('PDF created.');
-
-  return res.text(btoa(pdfBuffer));
+  const content = 'Some content!';
+  fs.writeFileSync('file.txt', content);
+  log("Writting ...")
+  const bytes = await fs.readFile('file.txt');
+  log("Sending ...")
+  return res.binary(bytes);
 };
